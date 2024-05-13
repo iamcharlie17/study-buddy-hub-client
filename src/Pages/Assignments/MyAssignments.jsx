cb1660ef684752ac740846611de6ac10181d2504
Riyad Hosen <riyadhosen@syn-172-220-155-060.res.spectrum.com>
2024-05-12 21:05:58 -0700

    added my-assignments page, 12 may, 9:05pm

--- src/Pages/Assignments/MyAssignments.jsx
+++ src/Pages/Assignments/MyAssignments.jsx
@@ -11,14 +11,62 @@ const MyAssignments = () => {
       setAssignments(data);
     });
   }, []);
-//   console.log(assignments.filter((a) => a.creator.email === user.email));
+  //   console.log(assignments.filter((a) => a.creator.email === user.email));
+
+  const MyAssignments = assignments.filter(
+    (a) => a.creator?.email === user?.email
+  );
 
   return (
     <div className="min-h-screen">
       <h1 className="pt-20 font-semibold text-4xl text-center text-[#045281]">
         My Assignments
       </h1>
-      <hr  className="mt-2"/>
+      <hr className="mt-2" />
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Marks</th>
+              <th>Due Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {MyAssignments.map((a) => (
+              <>
+                <tr>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-20 h-20">
+                          <img
+                            src={a.thumbnail}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{a.title}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td title={a.description}>
+                   {a.description.slice(0,30)}...
+                  </td>
+                  <td>{a.marks}</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">{a.dueDate}</button>
+                  </th>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
